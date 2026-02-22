3c3955ceab22466f1ea402ed9b0bd2fff40aceb9
Soawnjaja <basstraper@gmail.com>
2026-02-22 12:20:44 +0300

    trpc backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,20 +1,15 @@
-import express from 'express'
+import express, { Router } from 'express'
+import * as trpcExpress from '@trpc/server/adapters/express'
+import { trpcRoute } from './trpc'
 
-const ideas = [
-  { id: 'ideanick1', name: 'Idea1', description: 'Description idea1 ...' },
-  { id: 'ideanick2', name: 'Idea2', description: 'Description idea2 ...' },
-  { id: 'ideanick3', name: 'Idea3', description: 'Description idea3 ...' },
-  { id: 'ideanick4', name: 'Idea4', description: 'Description idea4 ...' },
-  { id: 'ideanick5', name: 'Idea5', description: 'Description idea5 ...' },
-]
 const expressApp = express()
 
-expressApp.get('/ping', (req, res) => {
-  res.send('pong')
-})
-expressApp.get('/ideas', (req, res) => {
-  res.send(ideas)
-})
+expressApp.use(
+  '/trpc',
+  trpcExpress.createExpressMiddleware({
+    router: trpcRouter,
+  })
+)
 expressApp.listen(3000, () => {
   console.info('Listening at http://localhost:3000')
 })
--- backend/src/trpc.ts
+++ backend/src/trpc.ts
@@ -1,5 +1,16 @@
 import { initTRPC } from '@trpc/server'
 
+const ideas = [
+  { id: 'ideanick1', name: 'Idea1', description: 'Description idea1 ...' },
+  { id: 'ideanick2', name: 'Idea2', description: 'Description idea2 ...' },
+  { id: 'ideanick3', name: 'Idea3', description: 'Description idea3 ...' },
+  { id: 'ideanick4', name: 'Idea4', description: 'Description idea4 ...' },
+  { id: 'ideanick5', name: 'Idea5', description: 'Description idea5 ...' },
+]
 const trpc = initTRPC.create()
 
-const trpcRouter = trpc.router()
+const trpcRouter = trpc.router({
+  getIdeas: trpc.procedure.query(() => {
+    return { ideas }
+  }),
+})
