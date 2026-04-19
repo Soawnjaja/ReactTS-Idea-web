ea986035e70bbc25e70904c9b5709f54faf54041
Soawnjaja <basstraper@gmail.com>
2026-04-19 19:15:04 +0300

    feat: route add idea

--- backend/src/router/createIdea/index.ts
+++ backend/src/router/createIdea/index.ts
@@ -1,11 +1,19 @@
-import { ideas } from '../../lib/ideas'
 import { trpc } from '../../lib/trpc'
 import { zCreateIdeaTrpcInput } from './input'
 
-export const createIdeaTrpcRoute = trpc.procedure.input(zCreateIdeaTrpcInput).mutation(({ input }) => {
-  if (ideas.find((idea) => idea.nick === input.nick)) {
-    throw Error('Idea with this nick already exists')
+export const createIdeaTrpcRoute = trpc.procedure.input(zCreateIdeaTrpcInput).mutation(async ({ ctx, input }) => {
+  const exIdea = await ctx.prisma.idea.findUnique({
+    where: {
+      nick: input.nick,
+    },
+  })
+
+  if (exIdea) {
+    throw new Error('Idea with this nick already exists')
   }
-  ideas.unshift(input)
-  return true
+
+  await ctx.prisma.idea.create({
+    data: input,
+  })
+  return { success: true }
 })
