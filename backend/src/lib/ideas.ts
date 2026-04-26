5664f01d193d3358a109d8c03439f097702efa2e
Soawnjaja <basstraper@gmail.com>
2026-04-26 11:12:13 +0300

    feat: update env

--- backend/src/lib/ideas.ts
+++
@@ -1,8 +0,0 @@
-import _ from 'lodash'
-
-export const ideas = _.times(100, (i) => ({
-  nick: `ideanick${i}`,
-  name: `Idea${i}`,
-  description: `Description idea${i} ...`,
-  text: _.times(100, (j) => `<p>Text paragraph ${j} of ides ${i} </p>`).join(''),
-}))
